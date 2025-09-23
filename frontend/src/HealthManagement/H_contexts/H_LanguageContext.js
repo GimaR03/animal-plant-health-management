3bbdd066a4fecc0f911b839f47e05bcac1c227b5
GimaR03 <it23673922@my.sliit.lk>
2025-09-23 21:48:24 +0530

    Update H_LanguageContext.js
    Update H_LanguageContext.js

--- frontend/src/HealthManagement/H_contexts/H_LanguageContext.js
+++ frontend/src/HealthManagement/H_contexts/H_LanguageContext.js
@@ -16,7 +16,7 @@ export const LanguageProvider = ({ children }) => {
   );
 };
 
-//  Custom hook
+// ✅ Custom hook
 export const useLanguage = () => {
   const context = useContext(LanguageContext);
   if (!context) throw new Error('useLanguage must be used inside LanguageProvider');
