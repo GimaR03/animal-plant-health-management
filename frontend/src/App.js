b103852324926f363ec3f62044e88c61399ca114
GimaR03 <it23673922@my.sliit.lk>
2025-09-27 22:29:35 +0530

    Update App.js
    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,3 +9,4 @@ import ScrollToTop from "./Components/UserHome/ScrollToTop/ScrollToTop.jsx";
 // ----------------- Context Providers -----------------
 import { AuthProvider } from "./Components/UserHome/UHContext/UHAuthContext.jsx";
 import { CartProvider } from "./Components/UserHome/UHContext/UHCartContext.jsx";
+import { ThemeProvider as UserThemeProvider } from "./Components/UserHome/UHContext/UHThemeContext.jsx";
