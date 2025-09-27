b6435e466bfd90c2c35b9f010e5447101023ad12
GimaR03 <it23673922@my.sliit.lk>
2025-09-27 22:31:10 +0530

    Update app.js
    Update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,3 +12,4 @@ import { CartProvider } from "./Components/UserHome/UHContext/UHCartContext.jsx"
 import { ThemeProvider as UserThemeProvider } from "./Components/UserHome/UHContext/UHThemeContext.jsx";
 import { LanguageProvider } from "./Components/AnimalManagement/contexts/LanguageContext.js";
 import { LoaderProvider } from "./Components/AnimalManagement/contexts/LoaderContext.js";
+import { ThemeProvider as AnimalThemeProvider } from "./Components/AnimalManagement/contexts/ThemeContext.js";
