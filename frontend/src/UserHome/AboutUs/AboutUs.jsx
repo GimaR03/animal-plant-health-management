b04d10b0efcbc175672d81ea93d7f5cc0be96291
GimaR03 <it23673922@my.sliit.lk>
2025-09-28 11:58:47 +0530

    Update AboutUs.jsx
    Update AboutUs.jsx

---
+++ frontend/src/UserHome/AboutUs/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import Navbar from '../UHNavbar/UHNavbar';
+import Footer from '../UHFooter/UHFooter';
+import { useTheme } from "../UHContext/UHThemeContext";
+import ChatBot from '../UHChatbot/UHChatbot';
+
+// Hero images (update paths or use URLs)
+import hero1 from "../Images/AboutUs1.jpg";
+import hero2 from "../Images/AboutUs2.jpg";
+import hero3 from "../Images/AboutUs3.jpg";
+import hero4 from "../Images/AboutUs4.jpg";
+
+// Team member images
+import ceoImg from "../Images/ceoAboutUs.jpg";
+import ctoImg from "../Images/CtoAboutUs.webp";
+import pmImg from "../Images/projectManagerAboutUs.jpeg";
