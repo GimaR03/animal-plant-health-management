2166b6f719aa8e8418af4f14f12320f490a29dec
GimaR03 <it23673922@my.sliit.lk>
2025-10-18 17:20:53 +0530

    Refactor to ES6 modules in company route
    Converted H_MedicineCompanyRoute.js from CommonJS to ES6 module syntax and updated controller imports. This improves code consistency and prepares for future ES6 features.

--- BACKEND/Routes/H_MedicineCompanyRoute.js
+++ BACKEND/Routes/H_MedicineCompanyRoute.js
@@ -1,12 +1,19 @@
-const express = require("express");
+// BACKEND/HealthManagement/Routes/H_MedicineCompanyRoute.js
+import express from "express";
+import {
+  addCompany,
+  getAllCompanies,
+  getCompanyById,
+  updateCompany,
+  deleteCompany,
+} from "../Controllers/H_MedicineCompanyController.js";
+
 const router = express.Router();
-const controller = require("../Controllers/H_MedicineCompanyController");
 
-// CRUD routes
-router.get("/", controller.getAllCompanies);
-router.get("/:id", controller.getCompanyById);
-router.post("/", controller.addCompany);
-router.put("/:id", controller.updateCompany);
-router.delete("/:id", controller.deleteCompany);
+router.get("/", getAllCompanies);
+router.get("/:id", getCompanyById);
+router.post("/", addCompany);
+router.put("/:id", updateCompany);
+router.delete("/:id", deleteCompany);
 
-module.exports = router;
+export default router;
